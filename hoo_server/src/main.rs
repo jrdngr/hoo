d40c87d5316b3e08547565c57d643c4a664cab8f
Jordan Grace <jordan.t.grace@gmail.com>
2018-12-01 15:56:57 -0500

    Trying to get actix-web set up

--- hoo_server/src/main.rs
+++ hoo_server/src/main.rs
@@ -1,3 +1,26 @@
+use actix_web::http::Method;
+use actix_web::{http, server, App, HttpRequest, HttpResponse, Path, Responder};
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| App::new().resource("/{id}", |r| r.method(Method::GET).f(index)))
+        .bind("127.0.0.1:8088")
+        .unwrap()
+        .run();
 }
+
+fn index(req: &HttpRequest) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("text/plain")
+        .body(format!("Hello {}!", req.match_info().get("name").unwrap()))
+}
+
+// fn index(_req: &HttpRequest) -> &'static str {
+//     "Hello world!"
+// }
+
+// fn main() {
+//     server::new(|| App::new().resource("/", |r| r.f(index)))
+//         .bind("127.0.0.1:8088")
+//         .unwrap()
+//         .run();
+// }
