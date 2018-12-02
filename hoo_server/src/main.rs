30d1d37ce299465eff903cacc47cc228cf22671b
Jordan Grace <jordan.t.grace@gmail.com>
2018-12-02 13:40:42 -0500

    on off api works

--- hoo_server/src/main.rs
+++ hoo_server/src/main.rs
@@ -1,26 +1,38 @@
 use actix_web::http::Method;
-use actix_web::{http, server, App, HttpRequest, HttpResponse, Path, Responder};
+use actix_web::{server, App, Path, Request, Result, State};
+use serde_derive::Deserialize;
+
+use hoohue_api::{api, api::ApiConnection};
 
 fn main() {
-    server::new(|| App::new().resource("/{id}", |r| r.method(Method::GET).f(index)))
-        .bind("127.0.0.1:8088")
-        .unwrap()
-        .run();
+    dotenv::dotenv().ok();
+
+    let base_uri = std::env::var("HUE_BASE_URI").expect("HUE_BASE_URI must be set");
+    let user_id = std::env::var("HUE_USER_ID").expect("HUE_USER_ID must be set");
+
+    server::new(move || {
+        App::with_state(AppState {
+            connection: ApiConnection::new(&base_uri, &user_id),
+        })
+        .resource("/on/{light_num}", |r| r.method(Method::GET).with(on))
+        .resource("/off/{light_num}", |r| r.method(Method::GET).with(off))
+        .finish()
+    })
+    .bind("127.0.0.1:8080")
+    .unwrap()
+    .run();
 }
 
-fn index(req: &HttpRequest) -> HttpResponse {
-    HttpResponse::Ok()
-        .content_type("text/plain")
-        .body(format!("Hello {}!", req.match_info().get("name").unwrap()))
+struct AppState {
+    connection: ApiConnection,
 }
 
-// fn index(_req: &HttpRequest) -> &'static str {
-//     "Hello world!"
-// }
+fn on(state: State<AppState>, info: Path<u8>) -> Result<String> {
+    api::on(&state.connection, *info).expect("Error turning on light");
+    Ok(format!("{} -> on", info))
+}
 
-// fn main() {
-//     server::new(|| App::new().resource("/", |r| r.f(index)))
-//         .bind("127.0.0.1:8088")
-//         .unwrap()
-//         .run();
-// }
+fn off(state: State<AppState>, info: Path<u8>) -> Result<String> {
+    api::off(&state.connection, *info).expect("Error turning off light");
+    Ok(format!("{} -> off", info))
+}
