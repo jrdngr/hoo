fa1330e07e13105c4a7594dced300a1b70b8c50b
Jordan Grace <jordan.t.grace@gmail.com>
2018-12-02 17:26:29 -0500

    Got server working for on/off

--- hoo/src/lib.rs
+++ hoo/src/lib.rs
@@ -1,3 +1,7 @@
+use std::sync::mpsc::{self, Receiver, Sender};
+use std::thread;
+
+use hoohue_api as api;
 use hoohue_api::ApiConnection;
 
 pub mod animation;
@@ -5,6 +9,79 @@ pub mod effects;
 
 pub type AnyError = Box<dyn std::error::Error>;
 
+type LightNumber = u8;
+type RgbValue = f64;
+type HueValue = u16;
+type SaturationValue = u8;
+type BrightnessValue = u8;
+type TransitionTime = u16;
+type HoldTime = u16;
+
 pub struct Hoo {
+    receiver: Receiver<HooCommand>,
     connection: ApiConnection,
+}
+
+impl Hoo {
+    pub fn new() -> (Self, Sender<HooCommand>) {
+        dotenv::dotenv().ok();
+
+        let base_uri = std::env::var("HUE_BASE_URI").expect("HUE_BASE_URI must be set");
+        let user_id = std::env::var("HUE_USER_ID").expect("HUE_USER_ID must be set");
+
+        let (sender, receiver) = mpsc::channel();
+
+        (
+            Hoo {
+                receiver,
+                connection: ApiConnection::new(&base_uri, &user_id),
+            },
+            sender,
+        )
+    }
+
+    pub fn run(&self) {
+        loop {
+            if let Ok(msg) = self.receiver.try_recv() {
+                println!("{:?}", msg);
+                match msg {
+                    HooCommand::On(light_num) => {
+                        api::on(&self.connection, light_num);
+                    }
+                    HooCommand::Off(light_num) => {
+                        api::off(&self.connection, light_num);
+                    }
+                    HooCommand::Quit => return,
+                    _ => println!("nah"),
+                }
+            }
+        }
+    }
+}
+
+impl Default for Hoo {
+    fn default() -> Self {
+        Hoo::new().0
+    }
+}
+
+#[derive(Debug, Clone)]
+pub enum HooCommand {
+    On(LightNumber),
+    Off(LightNumber),
+    Red(LightNumber, RgbValue),
+    Green(LightNumber, RgbValue),
+    Blue(LightNumber, RgbValue),
+    RgbColor(LightNumber, RgbValue, RgbValue, RgbValue),
+    Hue(LightNumber, HueValue),
+    Saturation(LightNumber, SaturationValue),
+    Brightness(LightNumber, BrightnessValue),
+    HsvColor(LightNumber, HueValue, SaturationValue, BrightnessValue),
+    ColorLoop(LightNumber, bool),
+    TransitionTime(LightNumber, TransitionTime),
+    // Animate(TransitionTime, HoldTime),
+    // Rainbow(Duration),
+    // Random(TransitionTime, HoldTime),
+    StopAnimation,
+    Quit,
 }
--- hoo_server/src/main.rs
+++ hoo_server/src/main.rs
@@ -1,35 +1,28 @@
-use std::sync::{mpsc, Arc, Mutex};
+use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
 
 use actix_web::http::Method;
-use actix_web::{server, App, Path, Query, Request, Result, State};
+use actix_web::{server, App, HttpRequest, Path, Query, Request, Result, State};
 use serde_derive::Deserialize;
 
 use hoo::animation::AnimationMessage;
 use hoo::effects;
-use hoohue_api::{self, ApiConnection};
+use hoo::{Hoo, HooCommand};
 
 fn main() {
     dotenv::dotenv().ok();
 
-    server::new(|| {
-        let base_uri = std::env::var("HUE_BASE_URI").expect("HUE_BASE_URI must be set");
-        let user_id = std::env::var("HUE_USER_ID").expect("HUE_USER_ID must be set");
+    let (hoo, sender) = Hoo::new();
 
-        let (sender, receiver) = mpsc::channel();
-        let receiver = Arc::new(Mutex::new(receiver));
+    thread::spawn(move || hoo.run());
 
-        App::with_state(AppState {
-            sender,
-            receiver,
-            connection: ApiConnection::new(&base_uri, &user_id),
-        })
-        .resource("/on/{light_num}", |r| r.method(Method::GET).with(on))
-        .resource("/off/{light_num}", |r| r.method(Method::GET).with(off))
-        .resource("anim", |r| r.method(Method::GET).with(animate))
-        .resource("stop", |r| r.method(Method::GET).with(stop_animation))
-        .finish()
+    server::new(move || {
+        App::with_state(AppState::new(&sender))
+            .resource("/on/{light_num}", |r| r.method(Method::GET).with(on))
+            .resource("/off/{light_num}", |r| r.method(Method::GET).with(off))
+            .resource("/stop", |r| r.method(Method::GET).with(stop))
+            .finish()
     })
     .bind("127.0.0.1:8080")
     .unwrap()
@@ -37,46 +30,30 @@ fn main() {
 }
 
 struct AppState {
-    sender: mpsc::Sender<AnimationMessage>,
-    receiver: Arc<Mutex<mpsc::Receiver<AnimationMessage>>>,
-    connection: ApiConnection,
+    sender: Sender<HooCommand>,
+}
+
+impl AppState {
+    pub fn new(sender: &Sender<HooCommand>) -> Self {
+        Self {
+            sender: sender.clone(),
+        }
+    }
 }
 
 fn on(state: State<AppState>, info: Path<u8>) -> Result<String> {
-    hoohue_api::on(&state.connection, *info).expect("Error turning on light");
+    let _ = state.sender.send(HooCommand::On(*info));
+
     Ok(format!("{} -> on", info))
 }
 
 fn off(state: State<AppState>, info: Path<u8>) -> Result<String> {
-    hoohue_api::off(&state.connection, *info).expect("Error turning off light");
+    let _ = state.sender.send(HooCommand::Off(*info));
+
     Ok(format!("{} -> off", info))
 }
 
-#[derive(Deserialize)]
-struct AnimateMessage {
-    tt: Option<u64>,
-    ht: Option<u64>,
-}
-
-fn animate(state: State<AppState>, info: Query<AnimateMessage>) -> Result<String> {
-    let transition_time = Duration::from_secs(info.tt.unwrap_or(1));
-    let hold_time = Duration::from_secs(info.ht.unwrap_or(0));
-
-    let anim = effects::rotate_current(
-        &state.connection,
-        &transition_time,
-        &hold_time,
-        state.receiver.clone(),
-    )
-    .expect("Error creating animation");
-
-    //thread::spawn(move || anim.play(&state.connection).expect("Error animating"));
-
-    Ok(format!("{:?} {:?}", info.tt, info.ht))
-}
-
-fn stop_animation(state: State<AppState>) -> Result<String> {
-    state.sender.send(AnimationMessage::Stop);
-
-    Ok(format!("Stopping animation"))
+fn stop(state: State<AppState>) -> Result<String> {
+    state.sender.send(HooCommand::Quit);
+    Ok("Stopping server".to_string())
 }
