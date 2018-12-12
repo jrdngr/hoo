bc796ab3d9229fe6ad252c1ede1bb1d4ba8552c7
Jordan Grace <jordan.t.grace@gmail.com>
2018-12-11 19:29:15 -0500

    Added a gradient above the hue slider

--- hoo_server/hoo-frontend/src/components/LightControls.vue
+++ hoo_server/hoo-frontend/src/components/LightControls.vue
@@ -6,6 +6,24 @@
       <button @click="off">Off</button>
     </div>
     <div class="control">
+      <svg id="hue-rainbow" width="130" height="10">
+        <defs>
+          <linearGradient id="hue-gradient" x1="0%" y1="50%" x2="100%" y2="50%">
+            <stop offset="0%" stop-color="hsl(0,100%,50%)"></stop>
+            <stop offset="10%" stop-color="hsl(36,100%,50%)"></stop>
+            <stop offset="20%" stop-color="hsl(72,100%,50%)"></stop>
+            <stop offset="30%" stop-color="hsl(108,100%,50%)"></stop>
+            <stop offset="40%" stop-color="hsl(144,100%,50%)"></stop>
+            <stop offset="50%" stop-color="hsl(180,100%,50%)"></stop>
+            <stop offset="60%" stop-color="hsl(216,100%,50%)"></stop>
+            <stop offset="70%" stop-color="hsl(252,100%,50%)"></stop>
+            <stop offset="80%" stop-color="hsl(288,100%,50%)"></stop>
+            <stop offset="90%" stop-color="hsl(324,100%,50%)"></stop>
+            <stop offset="100%" stop-color="hsl(360,100%,50%)"></stop>
+          </linearGradient>
+        </defs>
+        <rect width="130" height="10" fill="url(#hue-gradient)"></rect>
+      </svg>
       <input id="hue" type="range" min="0" max="65535" @input="hue">
       <label for="hue">Hue</label>
     </div>
@@ -68,5 +86,9 @@ export default {
   border: 1px solid gray;
   padding: 10px;
   width: 400px;
+}
+
+#hue-rainbow {
+  display: block;
 }
 </style>
