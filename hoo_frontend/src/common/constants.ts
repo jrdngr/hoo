0ee0c06f003cfec539d03271023d170b5d7e2e5b
Jordan Grace <jordan.t.grace@gmail.com>
2019-07-08 20:03:44 -0400

    Added /api to base api url

--- hoo_frontend/src/common/constants.ts
+++ hoo_frontend/src/common/constants.ts
@@ -1,4 +1,4 @@
 declare let process: any;
 
-export const BASE_URL = `http://${process.env.VUE_APP_IP}`;
+export const BASE_URL = `http://${process.env.VUE_APP_IP}/api`;
 export const INPUT_THROTTLING_DELAY = 100;
