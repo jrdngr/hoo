eb56252fdf13b51c90dfeff9c400938cb835661d
Jordan Grace <jordan.t.grace@gmail.com>
2019-09-29 14:37:52 -0400

    Added new project for frontend

--- hoo.sh
+++ hoo.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-cd hoo_frontend
-npm run build
-cd ..
 cargo run --release --bin hoo_server
--- hood.sh
+++ hood.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-cd hoo_frontend
-npm run build
-cd ..
 cargo run --bin hoo_server 
