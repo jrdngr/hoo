cf86b5f00116e58d71fcd93801c1ce3d3b6d883b
Jordan Grace <jordan.t.grace@gmail.com>
2020-01-01 16:13:17 -0500

    Added cli project, removed base and server projects

--- hoo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd hoo_frontend
-wasm-pack build --target web
-cd ..
-cargo run --release --bin hoo_server
--- hood.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd hoo_frontend
-wasm-pack build --target web
-cd ..
-cargo run --bin hoo_server 
