ad5859a81d3c56a83570a579bae81a700987f9ff
Jordan Grace <jordan.t.grace@gmail.com>
2020-01-02 22:18:13 -0500

    Added frontend crate

---
+++ hoo_frontend/src/lib.rs
@@ -0,0 +1,51 @@
+use seed::{*, prelude::*};
+use seed::browser::service::fetch;
+use hoo_api_types::LightCollection;
+
+use std::collections::HashMap;
+
+struct Model {
+    pub lights: LightCollection,
+}
+
+impl Default for Model {
+    fn default() -> Self {
+        Self {
+            lights: HashMap::new(),
+        }
+    }
+}
+
+#[derive(Clone)]
+enum Msg {
+    GetAllLights,
+    GetAllLightsFetched(fetch::ResponseDataResult<LightCollection>),
+}
+
+fn update(msg: Msg, _model: &mut Model, orders: &mut impl Orders<Msg>) {
+    match msg {
+        Msg::GetAllLights => orders.skip().perform_cmd(get_all_lights()),
+        Msg::GetAllLightsFetched(Ok(_lights)) => orders.skip(),
+        Msg::GetAllLightsFetched(Err(_e)) => orders.skip(),
+    };
+}
+
+fn view(model: &Model) -> impl View<Msg> {
+    button![
+        simple_ev(Ev::Click, Msg::GetAllLights),
+        format!("Hello, World × {}", model.lights.len())
+    ]
+}
+
+async fn get_all_lights() -> Result<Msg, Msg> {
+    Request::new("http://localhost:8000/api/lights")
+        .method(Method::Get)
+        .fetch_json_data(Msg::GetAllLightsFetched)
+        .await
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    App::builder(update, view)
+        .build_and_start();
+}
