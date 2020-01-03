51575483d7a3aa86e87e85cbe84abba4616eaf52
Jordan Grace <jordan.t.grace@gmail.com>
2020-01-02 19:38:22 -0500

    Path handling now uses an iterator

--- hoo_server/src/main.rs
+++ hoo_server/src/main.rs
@@ -1,13 +1,10 @@
 mod options;
-mod utils;
 
 use anyhow::Result;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use structopt::StructOpt;
 use hoo_api::HueClient;
-
-use utils::next_path_component;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -37,39 +34,43 @@ async fn main() -> Result<()> {
 }
 
 async fn handle(req: Request<Body>, client: HueClient) -> Result<Response<Body>> {
-    let path = req.uri().path();
-
-    match next_path_component(path) {
-        Some(("api", endpoint)) => handle_api(req.method(), endpoint, client).await,
+    let mut path = req.uri()
+        .path()
+        .trim_start_matches('/')
+        .split('/')
+        .into_iter();
+    
+    match path.next() {
+        Some("api") => handle_api(req.method(), path, client).await,
         _ => Ok(not_found())
     }
 }
 
-async fn handle_api(method: &Method, endpoint: &str, client: HueClient) -> Result<Response<Body>> {
-    let (endpoint, rest) = next_path_component(endpoint).expect(&format!("Invalid path: {}", endpoint));
-    match (method, endpoint) {
-        (&Method::GET, "lights") => client.get_all_lights_response().await,
-        (&Method::GET, "light") => {
-            match next_path_component(rest) {
-                Some((light_num, "")) => {
-                    let light_num = light_num.parse().expect("Invalid light number");
-                    client.get_light_response(light_num).await
-                },
-                Some((light_num, path)) => handle_light_state(method, light_num, path, client).await,
-                None => Ok(not_found()),
-            }
-        },
-        _ => Ok(not_found())
+async fn handle_api(method: &Method, mut path: impl Iterator<Item = &str>, client: HueClient) -> Result<Response<Body>> {
+    match path.next() {
+        None => Ok(not_found()),
+        Some(endpoint) => match (method, endpoint) {
+            (&Method::GET, "lights") => client.get_all_lights_response().await,
+            (&Method::GET, "light") => {
+                match path.next() {
+                    Some(light_num) => handle_light_state(method, light_num, path, client).await,
+                    None => Ok(not_found()),
+                }
+            },
+            _ => Ok(not_found())
+        }
     }
 }
 
-async fn handle_light_state(method: &Method, light_num: &str, path: &str, client: HueClient) -> Result<Response<Body>> {
+async fn handle_light_state(method: &Method, light_num: &str, mut path: impl Iterator<Item = &str>, client: HueClient) -> Result<Response<Body>> {
     let light_num: u8 = light_num.parse().expect("Invalid light number");
-    let (command, _) = next_path_component(path).expect(&format!("Invalid path: {}", path));
-    match (method, command) {
-        (&Method::PUT, "on") => client.on(light_num).await,
-        (&Method::PUT, "off") => client.off(light_num).await,
-        _ => Ok(not_found())
+    match path.next() {
+        None => client.get_light_response(light_num).await,
+        Some(command) => match (method, command) {
+            (&Method::GET/*PUT*/, "on") => client.on(light_num).await,
+            (&Method::GET/*PUT*/, "off") => client.off(light_num).await,
+            _ => Ok(not_found())
+        }
     }
 }
 
