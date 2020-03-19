1b6852ab1aa8fa6995126235cd89b67afb065cee
Jordan Grace <jordan.t.grace@gmail.com>
2020-03-19 14:57:36 -0400

    Moved some stuff over to warp

--- hoo_server/src/main.rs
+++ hoo_server/src/main.rs
@@ -1,10 +1,11 @@
 mod options;
 
 use anyhow::{anyhow, Result};
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use structopt::StructOpt;
+use warp::Filter;
+
 use hoo_api::HueClient;
+use hoo_api_types::LightState;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -14,81 +15,32 @@ async fn main() -> Result<()> {
     let addr = "127.0.0.1:8000".parse().unwrap();
 
     let client = HueClient::new(&options.hue_base_uri, &options.hue_user_id);
-
-    let new_service = make_service_fn(move |_| {
-        let client = client.clone();
-        async {
-            Ok::<_, anyhow::Error>(service_fn(move |req| {
-                handle(req, client.to_owned())
-            }))
-        }
-    });
-
-    let server = Server::bind(&addr).serve(new_service);
-
     println!("Hoo server listening on http://{}", addr);
 
-    server.await?;
+    let preflight = warp::options().map(|| preflight());
+    let all_lights = warp::get().and(warp::path("lights").map(|| client.get_all_lights_response()));
+    
+    let light =
+        warp::get().and(warp::path!("light" / u8).map(|light_num| client.get_light_response(light_num)))
+        .or(
+            warp::put().and(
+                warp::path!(u8/ "on").map(|light_num| client.on(light_num))
+                .or(warp::path!(u8 / "off").map(|light_num| client.off(light_num)))
+                .or(warp::path!(u8 / "toggle").map(|light_num| client.toggle(light_num)))
+                .or(warp::path!(u8 / "state").and(warp::body::json()).map(|light_num, state: LightState| client.set_state(light_num, &state)))
+            )
+    );
+
+    let routes = warp::path("api")
+        .and(
+            preflight
+            // .or(all_lights)
+            // .or(light)
+        );
+    
+    warp::serve(routes).run(addr).await;
 
     Ok(())
-}
-
-async fn handle(req: Request<Body>, client: HueClient) -> Result<Response<Body>> {
-    let path = req.uri().path().to_string();
-    println!("Routing: {}", path);
-
-    let mut path = path
-        .trim_start_matches('/')
-        .split('/')
-        .into_iter();
-    
-    let result = match path.next() {
-        Some("api") => handle_api(req, path, client).await,
-        _ => Ok(not_found()),
-    };
-
-    match result {
-        Ok(response) => Ok(response),
-        Err(e) => { 
-            dbg!(e);
-            Ok(internal_server_error())
-        },
-    }
-}
-
-async fn handle_api(req: Request<Body>, mut path: impl Iterator<Item = &str>, client: HueClient) -> Result<Response<Body>> {
-    match path.next() {
-        None => Ok(not_found()),
-        Some(endpoint) => match (req.method(), endpoint) {
-            (&Method::OPTIONS, _) => Ok(preflight()),
-            (&Method::GET, "lights") => client.get_all_lights_response().await,
-            (&Method::GET, "light") =>  { 
-                let light_num: u8 = path.next()
-                    .ok_or(anyhow!("Missing light number"))?
-                    .parse()?;
-                client.get_light_response(light_num).await 
-            },
-            (&Method::PUT, "light") =>  handle_light_state(req, path, client).await,
-            _ => Ok(not_found())
-        }
-    }
-}
-
-async fn handle_light_state(req: Request<Body>, mut path: impl Iterator<Item = &str>, client: HueClient) -> Result<Response<Body>> {
-    let light_num: u8 = path.next()
-        .ok_or(anyhow!("Missing light number"))?
-        .parse()?;
-
-    match path.next() {
-        Some(command) => match (req.method(), command) {
-            (&Method::PUT, "on") => client.on(light_num).await,
-            (&Method::PUT, "off") => client.off(light_num).await,
-            (&Method::PUT, "toggle") => client.toggle(light_num).await,
-            (&Method::PUT, "state") => client.set_state_from_body(light_num, req.into_body()).await,
-            _ => Ok(not_found())
-        }
-        _ => Ok(not_found())
-    }
 }
 
 fn preflight() -> Response<Body> {
